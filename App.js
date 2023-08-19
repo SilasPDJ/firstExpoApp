ad4ad23e450c9fc9acce3d1d9221af2f1c020ab3
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-08-18 21:57:01 -0300

    18/08/2023 finished friday class

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as NavigationBar from 'expo-navigation-bar';
+import MobiButton from './src/components/MobiButton/MobiButton';
+
+const mostrarTexto = () => {
+  console.log("Olá, tudo bem?")
+}
+
 
 export default function App() {
+  const visibility = NavigationBar.useVisibility();
+  NavigationBar.setBackgroundColorAsync("white");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Meu Primeiro App! OLA</Text>
+
+      <MobiButton title="Olá, botão!" funcaoOnPress={mostrarTexto}>
+        Primeiro Botão
+      </MobiButton>
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -13,8 +28,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#35A',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  texto: {
+    color: "red",
+    fontWeight: 'bold',
+    fontSize: 32,
+  }
 });
